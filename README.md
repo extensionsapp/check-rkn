5d4f421e1b1377f3777834b25d6b855033bcfdf6
Surinam <extensionsapp@gmail.com>
2018-05-11 22:20:35 +0300

    Add check IPs in domain

--- README.md
+++ README.md
@@ -12,23 +12,27 @@ npm i check-rkn
 ```javascript
 const rkn = require('check-rkn');
 
-rkn(['105.28.12.143', '104.28.12.143'], (err, res) => {
+// Check list IPs
+rkn(['216.58.214.110', '108.174.10.10'], (err, res) => {
     console.log(res)
 });
-// [ { ip: '105.28.12.143', block: false },
-//   { ip: '104.28.12.143', block: true  } ]
+// [ { ip: '216.58.214.110', block: false },
+//   { ip: '108.174.10.10', block: true  } ]
 
-rkn('104.28.12.143', (err, res) => {
+// Check one IP
+rkn('108.174.10.10', (err, res) => {
     console.log(res)
 });
 // true
 
+// Check list domains
 rkn(['google.com', 'linkedin.com'], (err, res) => {
     console.log(res)
 });
-// [ { ip: 'google.com', block: false },
-//   { ip: 'linkedin.com', block: true  } ]
+// [ { ip: 'google.com', block: false, ips: [ { ip: '216.58.214.110',  block: false } ] },
+//   { ip: 'linkedin.com', block: true, ips: [ { ip: '108.174.10.10',  block: true } ] } ]
 
+// Check one domain (and IP)
 rkn('linkedin.com', (err, res) => {
     console.log(res)
 });
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const https = require('https');
+const async = require('async');
 const path  = require('path');
+const dns   = require('dns');
 const fs    = require('fs');
 const ip    = require('ip');
 
 let prefix = (new Date()).getDate() + '-' + (new Date()).getHours();
 
-const base = {
+let base = {
     "ips": (fs.existsSync(path.join(__dirname, 'ips' + prefix + '.json')))
         ? JSON.parse((fs.readFileSync(path.join(__dirname, 'ips' + prefix + '.json'), 'utf8')))
         : [],
@@ -19,30 +21,80 @@ const base = {
 module.exports = check_rkn;
 
 function check_rkn(check_data, callback) {
-    let type = (typeof check_data === 'object' && check_data[0])
-        ? (ip.isV4Format(check_data[0]))
-            ? 'ips'
-            : 'domains'
-        : (ip.isV4Format(check_data))
-            ? 'ips'
-            : 'domains';
-    if (!base[type].length) {
+    let type = typeof check_data !== 'object'
+        ? ip.isV4Format(check_data) ? 'ip' : 'domain'
+        : false;
+    check_data = typeof check_data === 'object' ? check_data : [check_data];
+    if (!base.ips.length || !base.domains.length) {
         clear();
-        get(type, err => {
+        get('ips', err => {
             if (err) return callback(err);
-            base[type] = (fs.existsSync(path.join(__dirname, type + prefix + '.json')))
-                ? JSON.parse((fs.readFileSync(path.join(__dirname, type + prefix + '.json'), 'utf8')))
-                : [];
-            check(type, check_data, base[type], (err, res) => {
-                callback(err, res);
+            get('domains', err => {
+                if (err) return callback(err);
+                base['ips'] = (fs.existsSync(path.join(__dirname, 'ips' + prefix + '.json')))
+                    ? JSON.parse((fs.readFileSync(path.join(__dirname, 'ips' + prefix + '.json'), 'utf8')))
+                    : [];
+                base['domains'] = (fs.existsSync(path.join(__dirname, 'domains' + prefix + '.json')))
+                    ? JSON.parse((fs.readFileSync(path.join(__dirname, 'domains' + prefix + '.json'), 'utf8')))
+                    : [];
+                check(check_data, type, (err, res) => {
+                    callback(err, res);
+                })
             })
         })
     }
     else {
-        check(type, check_data, base[type], (err, res) => {
+        check(check_data, type, (err, res) => {
             callback(err, res);
         })
     }
+}
+
+function check(check_data, type, callback) {
+    async.forEachOf(check_data, function (value, key, callback) {
+        if (!ip.isV4Format(value)) {
+            dns.resolve4(value, (err, addresses) => {
+                if (err) {
+                    check_data.splice(key, 1, {
+                        "domain": value,
+                        "error": err.code
+                    });
+                }
+                else {
+                    check_data.splice(key, 1, {
+                        "domain": value,
+                        "block": !!(base.domains.filter(d => d === value)).length,
+                        "ips": addresses.map(address => {
+                            return {
+                                "ip": address,
+                                "block": !!(base.ips.filter(i =>
+                                    ip.cidrSubnet(i.indexOf('/')+1?i:i+'/32').contains(address))).length
+                            }
+                        })
+                    });
+                }
+                callback()
+            })
+        }
+        else {
+            check_data.splice(key, 1, {
+                "ip": value,
+                "block": !!(base.ips.filter(i =>
+                    ip.cidrSubnet(i.indexOf('/')+1?i:i+'/32').contains(value))).length
+            });
+            callback()
+        }
+    }, function () {
+        if (type) {
+            if (type === 'ip') {
+                check_data = check_data[0].block;
+            }
+            else {
+                check_data = check_data[0].block || !!(check_data[0].ips.filter(d => d.block)).length
+            }
+        }
+        callback(null, check_data)
+    })
 }
 
 function get(type, callback) {
@@ -66,33 +118,6 @@ function get(type, callback) {
         .on('error', err => {
             callback(err)
         });
-}
-
-function check(type, check_data, data, callback) {
-    if (typeof check_data === 'object') {
-        if (type === 'ips') {
-            callback(null, check_data.map(check_d => {
-                let res = data.filter(d => ip.cidrSubnet(d.indexOf('/')+1?d:d+'/32').contains(check_d));
-                return {"ip": check_d, "block": !!res.length}
-            }));
-        }
-        else {
-            callback(null, check_data.map(check_d => {
-                let res = data.filter(d => d === check_d);
-                return {"domain": check_d, "block": !!res.length}
-            }));
-        }
-    }
-    else {
-        if (type === 'ips') {
-            let res = data.filter(d => ip.cidrSubnet(d.indexOf('/')+1?d:d+'/32').contains(check_data));
-            callback(null, !!res.length);
-        }
-        else {
-            let res = data.filter(d => d === check_data);
-            callback(null, !!res.length);
-        }
-    }
 }
 
 function clear() {
